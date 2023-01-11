cb18148a67daddfea23de391271fc66b57b04c2c
ChenLipschitz <93311801+ChenLipschitz@users.noreply.github.com>
2023-01-11 21:26:23 +0200

    Update CustomExecutor.java

--- src/part2/CustomExecutor.java
+++ src/part2/CustomExecutor.java
@@ -1,48 +1,68 @@
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.concurrent.*;
 
 public class CustomExecutor <T> extends ThreadPoolExecutor{
-
-    private HashMap<Integer, Integer> priorities;   //key- priority number, value- the number of tasks with that priority
+    private int[] arrPriorities = {0, 0, 0};
+    //index- represents the priority(-1),
+    // arrPriorities[i]- represents the number of tasks with i+1 priority
     private static final int MIN_NUM_OF_THREADS = Runtime.getRuntime().availableProcessors()/2;
     private static final int MAX_NUM_OF_THREADS = Runtime.getRuntime().availableProcessors()-1;
 
-
+    //constructor
     public CustomExecutor() {
         super(MIN_NUM_OF_THREADS,
                 MAX_NUM_OF_THREADS,
-                3000,
+                300,
                 TimeUnit.MILLISECONDS,
-                new PriorityBlockingQueue<>(MIN_NUM_OF_THREADS,
-                        (task1, task2) -> ((Task)task1).compareTo((Task) task2)));
-        setCounts();
+                new PriorityBlockingQueue<>());
     }
 
     /**
-     * initiates the hashmap
-      */
-    private void setCounts(){
-        priorities.put(1,0);
-        priorities.put(2,0);
-        priorities.put(3,0);
+     *
+     * @param task
+     * @param <T>
+     * @return
+     */
+    protected <T> MyFutureTask<T> newTask(Task task) {
+        return new MyFutureTask<T>(task.getCall(), task.getPriority());
     }
 
-
-
+    /**
+     *
+     * @param task
+     * @param <T>
+     * @return
+     */
     public <T> Future<T> submit(Task<T> task) {
-        FutureTask futureTask = new FutureTask(task);
-        int currPriority = priorities.get(task.getPriority());
-        priorities.put(task.getPriority(), currPriority+1);
-        return (Future<T>) futureTask;
+        if (task == null){
+            throw new NullPointerException();
+        }
+//        RunnableFuture<T> futureTask = newTaskFor(task);
+        MyFutureTask<T> futureTask = newTask(task);
+        if (task.getPriority()>0)
+            arrPriorities[task.getPriority()-1]++;
+        execute(futureTask);
+        return futureTask;
+
     }
 
-
+    /**
+     *
+     * @param callable
+     * @param <T>
+     * @return
+     */
     public <T> Future<T> submit(Callable<T> callable){
         Task<T> task = Task.createTask(callable);
         return submit(task);
     }
 
+    /**
+     *
+     * @param callable
+     * @param type TaskType object
+     * @param <T> generic type
+     * @return
+     */
     public <T> Future<T> submit(Callable<T> callable, TaskType type) {
         Task<T> task = Task.createTask(callable, type);
         return submit(task);
@@ -54,14 +74,42 @@ public class CustomExecutor <T> extends ThreadPoolExecutor{
      * @return 1 or 2 or 3, the max priority in the queue, 1 is the highest
      */
     public int getCurrentMax(){
-        if (priorities.get(1) > 0)
-            return 1;
-        if (priorities.get(2) > 0)
-            return 2;
-        if (priorities.get(3) > 0)
-            return 3;
+        try {
+            if (arrPriorities[0] > 0)
+                return 1;
+            if (arrPriorities[1] > 0)
+                return 2;
+            if (arrPriorities[2] > 0)
+                return 3;
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
         return 0;
     }
 
+    /**
+     *
+     * @param r
+     * @param t
+     */
+    @Override
+    protected void afterExecute(Runnable r, Throwable t) {
+        super.afterExecute(r, t);
+        if (getCurrentMax() > 0){
+            arrPriorities[getCurrentMax()-1]--;
+        }
+    }
 
+    /**
+     *
+     * @throws InterruptedException
+     */
+    public void gracefullyTerminate() throws InterruptedException {
+         try{
+             awaitTermination(300, TimeUnit.MILLISECONDS);
+             super.shutdown();
+         }catch (InterruptedException e){
+             System.out.println("Got InterruptedException: "+e.getMessage());
+         }
+    }
 }
