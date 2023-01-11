a0ecfb4ca3839af5f3e83cb6ecab9e2188797103
ChenLipschitz <93311801+ChenLipschitz@users.noreply.github.com>
2023-01-11 21:25:11 +0200

    updated javadocs

--- src/part2/MyFutureTask.java
+++ src/part2/MyFutureTask.java
@@ -1,10 +1,17 @@
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 
+/**
+ * The following class is used as an adapter class.
+ * The FutureTask class objects can not be compared, the problem is that the objects that are passed to the priority queue (submit method)
+ * must be comparable objects.
+ * The MyFutureTask class allows us to submit FutureTask objects that are comparable to the priority queue
+ */
 class MyFutureTask<V> extends FutureTask<V> implements Comparable<MyFutureTask> {
 
     private int priority = 0;
 
+    //constructor
     public MyFutureTask(Callable<V> callable, int priority) {
         super(callable);
         this.priority = priority;
@@ -19,8 +26,7 @@ class MyFutureTask<V> extends FutureTask<V> implements Comparable<MyFutureTask> 
 
 
     /**
-     * Override method that cast the Task class to be
-     * a comparable class
+     * compares between two MyFutureTask objects
      * @param other MyFutureTask object
      * @return 0, if the priorities are equal
      *         1, if this object has a greater priority
